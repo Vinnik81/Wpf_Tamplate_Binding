bbb738e74d6961768f8cd612e3849ce59338ba51
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-10-05 20:29:15 +0600

    Binding DONE

--- WpfApp_DataBinding/MainWindow.xaml.cs
+++ WpfApp_DataBinding/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,56 @@ namespace WpfApp_DataBinding
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        //public string MyText { get; set; } = "Hello 2022";
+
+        private string myText;
+
+        public string MyText { get => myText; set => OnChanged(out myText, value); }
+        private string phone;
+
+        public string Phone { get => phone; set => OnChanged(out phone, value); }
+       
+
+        //private string myText;
+
+        //public string MyText
+        //{
+        //    get 
+        //    {
+        //        return myText; 
+        //    }
+        //    set 
+        //    { 
+        //        myText = value;
+        //        OnChanged(out myText, value);
+        //        //OnChanged(out myText, value, "MyText");
+        //        //PropertyChanged.Invoke(this, new PropertyChangedEventArgs("MyText"));
+        //    }
+        //}
+
+        private void OnChanged<T>(out T prop, T value,[CallerMemberName] string propName = "")
+        {
+            prop = value;
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            MyText = "Bye " + random.Next(2015,2022);
+            //MessageBox.Show("bye");
+            //DataContext = null;
+            //DataContext = this;
         }
 
         //private void textBoxInput_TextChanged(object sender, TextChangedEventArgs e)
